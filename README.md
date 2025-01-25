6b0898e4cb8cb0f4d4673552e675a0f3fcc369ed
DiegoRuidias <162191324+DiegoRuidias@users.noreply.github.com>
2025-01-24 19:06:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Sis_Ventas_Back" 
---
+++ src/test/java/com/system/ventas/VentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
