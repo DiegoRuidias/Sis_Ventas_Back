d23bae3ff9475c6fb53460f08c1ab9f1d18b92b8
DiegoRuidias <162191324+DiegoRuidias@users.noreply.github.com>
2025-02-04 23:01:47 -0500

    fixed id de sales

--- src/main/java/com/system/ventas/model/entities/Sales.java
+++ src/main/java/com/system/ventas/model/entities/Sales.java
@@ -1,6 +1,5 @@
 package com.system.ventas.model.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
